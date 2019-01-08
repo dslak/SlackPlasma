6cc0161d3c066d0513cdb3f6102c9cf3a8a5d958
Carmine De Rosa <dslaky@gmail.com>
2019-01-08 17:08:18 +0100

    Moved to GitLab

--- 3_install.sh
+++
@@ -1,4 +0,0 @@
-upgradepkg --reinstall --install-new ../5/x86*/deps/*.t?z
-upgradepkg --reinstall --install-new ../5/x86*/deps/telepathy/*.t?z
-upgradepkg --reinstall --install-new ../5/x86*/kde/*/*.t?z
-upgradepkg --reinstall --install-new ../5/x86*/kdei/kde-l10n-it*.t?z
